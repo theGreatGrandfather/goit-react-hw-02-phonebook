f1e085b8949e9ac4ba23b2103a9aa02d01bdba78
Praded3 <pradid.store@gmail.com>
2023-08-19 13:59:17 +0300

    12

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
-import styled from '@emotion/styled'
-
+ 
 import  App  from 'components/App';
  
 
